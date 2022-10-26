a49aad854ab7bd324bb31bd4d4d2be8a9904636a
oriolli <mar.orioli@hotmail.com>
2022-10-26 09:14:30 -0300

    Iniciando com valores

--- Program.cs
+++ Program.cs
@@ -7,10 +7,17 @@ namespace Calculator
     static void Main(string[] args)
     {
       Console.Clear();
+
       Console.WriteLine("Primeiro valor: ");
       float v1 = float.Parse(Console.ReadLine());
 
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      float resultado = v1 + v2;
+      Console.WriteLine("");
       Console.WriteLine(v1);
+      Console.WriteLine("O resultado da soma é " + resultado);
     }
   }
 }
